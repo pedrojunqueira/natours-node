dc4ab8a4bf9566e96b11b1f2ce946bf86676834e
Pedro Junqueira <pedrocj@gmail.com>
2021-11-11 06:49:09 +1030

    added simple middleware

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ const app = express();
 
 app.use(express.json());
 
+// adding middleware
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 🧸');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toDateString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -67,12 +80,9 @@ const deleteTour = (req, res) => {
     data: null,
   });
 };
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
